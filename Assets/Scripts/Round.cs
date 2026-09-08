68735ba2df1c323ac37ee8c8cf3057d8470b8a3e
wbermudez06-cmd <wbermudez06@gmail.com>
2026-09-08 13:17:32 +0200

    Update Round.cs

--- Assets/Scripts/Round.cs
+++ Assets/Scripts/Round.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagment;
+using UnityEngine.SceneManagement;
 
 
 // all  code in this file is conceptual and does not rely on the implementation of other code yet
