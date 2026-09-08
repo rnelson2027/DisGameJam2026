8c23519b4a5e79e6e79fe316280845d0d1b6c2a7
wbermudez06-cmd <wbermudez06@gmail.com>
2026-09-08 12:53:26 +0200

    Update Round.cs

--- Assets/Scripts/Round.cs
+++ Assets/Scripts/Round.cs
@@ -3,6 +3,7 @@ using TMPro;
 using System.Collections;
 using UnityEngine.InputSystem;
 
+
 // all  code in this file is conceptual and does not rely on the implementation of other code yet
 public class Round : MonoBehaviour
 {
@@ -17,22 +18,50 @@ public class Round : MonoBehaviour
     [Header("Round Settings")]
     [SerializeField] float roundTime = 10f;
     [SerializeField] int maxRounds = 10; 
-    [SerializeField] float outcomeThreshold = 10f;
+    //[SerializeField] float outcomeThreshold = 10f;
 
     private int currentRound;
     private float totalDisparity;
     private float disparity;
 
+    private Vector2 chairOriginalPosition;
+
+    private InputAction spaceBarAction;
+
+    private bool roundFinished;
+
     // Update is called once per frame
 
     void Start()
     {
-        NewRound();
+        chairOriginalPosition = chair.transform.position;
+
+        spaceBarAction = InputSystem.actions.FindAction("Jump");
+        UpdateRoundText();
+
+    }
+
+    void Update()
+    {
+        // Round.cs does NOT move the chair.
+        // MoveAndCheck still handles that.
+
+        // We only care about the Space RELEASE.
+        if (!roundFinished && spaceBarAction.WasReleasedThisFrame())
+        {
+            roundFinished = true;
+
+            StartCoroutine(FinishRound());
+        }
     }
 
     private void NewRound()
     {
         currentRound++;
+
+        chair.transform.position = chairOriginalPosition;
+
+        person.transfor.position = new Vector2(person.transform.position.x, Random.Range(3f, 10f));
 
         if (roundText != null)
         {
@@ -44,45 +73,60 @@ public class Round : MonoBehaviour
             disparityText.text = "";
         }
 
-        StartCoroutine(RoundTimer());
+        UpdateRoundText();
+
+        roundFinished = false;
     }
 
-    private IEnumerator RoundTimer()
+
+    private IEnumerator FinishRound()
     {
-        yield return new WaitForSeconds(roundTime);
-        // sprite sits down
+        // NOT IMPLEMENTED YET:
+        // Make the person sprite sit down here.
+
+
+        // Calculate how accurate the chair placement was
         CalculateDisparity();
 
+        // Add this round to the total
         totalDisparity += disparity;
 
+
+        // Show this round's result
         if (disparityText != null)
         {
-        disparityText.text = "Disparity: " + disparity.ToString("F2");
+            disparityText.text =
+                "Disparity: " + disparity.ToString("F2");
         }
 
+
+        // Keep the score visible for 2 seconds
         yield return new WaitForSeconds(2f);
+
 
         if (currentRound < maxRounds)
         {
-            yield return StartCoroutine(WaitForSpace());
-            person.transform.position = new Vector2(person.transform.position.x, Random.Range(3f, 10f));
             NewRound();
         }
         else
         {
-            /// add end game options
+            EndGame();
         }
     }
 
-    private IEnumerator WaitForSpace()
-    {
-        yield return new WaitUntil(() => Keyboard.current != null && Keyboard.current.spaceKey.IsPressed());
-        yield return new WaitUntil(() => Keyboard.current != null && !Keyboard.current.spaceKey.IsPressed());
-    }
-    
     private void CalculateDisparity()
     {
         disparity = Mathf.Abs(chair.referencePoint.position.y - person.referencePoint.position.y);
     }
+
+    private void UpdateRoundText()
+    {
+        if (roundText != null)
+        {
+            roundText.text =
+                "Round " + currentRound + " / " + maxRounds;
+        }
+    }
+
 
 }
